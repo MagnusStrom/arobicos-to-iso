6fa2d091a8fe5f86cc745d2a5b7820dffa227caf
MagnusStrom <80227480+MagnusStrom@users.noreply.github.com>
2021-07-20 11:22:38 -0400

    Update autostart.sh

--- iso_scripts/autostart.sh
+++ iso_scripts/autostart.sh
@@ -11,3 +11,5 @@ hsetroot -add "#0E5CA8" -add "#87C6C9" -gradient 0 -center /usr/local/share/pixm
 [ $(which "$ICONS".sh) ] && ${ICONS}.sh &
 [ -d "/usr/local/etc/X.d" ] && find "/usr/local/etc/X.d" -type f -o -type l -print | while read F; do . "$F"; done
 [ -d "$HOME/.X.d" ] && find "$HOME/.X.d" -type f -print | while read F; do . "$F"; done
+
+wmctrl -r ':ACTIVE:' -b toggle,fullscreen
