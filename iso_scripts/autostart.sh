1dd99d56322666dfcc5572a0f2cb8d0a78b879ee
MagnusStrom <80227480+MagnusStrom@users.noreply.github.com>
2021-07-20 18:29:35 -0400

    Revert "Update autostart.sh"
    This reverts commit 770fb78addc8a9894c14980caf255dc68f318b5e.

--- iso_scripts/autostart.sh
+++ iso_scripts/autostart.sh
@@ -12,6 +12,4 @@ hsetroot -add "#000000" -add "#000000" -gradient 0 -center /usr/local/share/pixm
 [ -d "/usr/local/etc/X.d" ] && find "/usr/local/etc/X.d" -type f -o -type l -print | while read F; do . "$F"; done
 [ -d "$HOME/.X.d" ] && find "$HOME/.X.d" -type f -print | while read F; do . "$F"; done
 
-FILE="~/.config/openbox/rc.xml"
-echo "*** File - $FILE contents ***"
-cat $FILE
+wmctrl -r ':ACTIVE:' -b toggle,fullscreen
